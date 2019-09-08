60ccd299222655eb089a57afcbee6d3031dc49fe
Jakob Buis <jakob@jakobbuis.nl>
2019-09-08 23:22:18 +0200

    add a JIRA class to parse tickets from commit messages

---
+++ app/JIRA.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Collection;
+
+class JIRA
+{
+    /**
+     * Search all messages for valid ticket references, and build a collection
+     * of the relevant details.
+     */
+    public function parseTickets(Collection $messages, string $projectCode) : Collection
+    {
+        // Ticket IDs take the shape of the project identifier + a number
+        $regex = '/(' . $projectCode . '\-\d+)/i';
+
+        $tickets = collect();
+
+        foreach ($messages as $message) {
+            $matches = [];
+            if (!preg_match($regex, $message, $matches)) {
+                continue;
+            }
+
+            $tickets->push((object) [
+                'ticket' => $matches[1],
+                'message' => $message,
+            ]);
+        }
+
+        return $tickets;
+    }
+}
