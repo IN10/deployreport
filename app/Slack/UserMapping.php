551a325fd58d43875d87f2981241830c8a63eef6
Jakob Buis <jakob@jakobbuis.nl>
2019-09-09 22:52:39 +0200

    add all Slack IDs for existing devteam members

--- app/Slack/UserMapping.php
+++ app/Slack/UserMapping.php
@@ -9,7 +9,16 @@ namespace App\Slack;
 class UserMapping
 {
     private static $mapping = [
+        'basdebeer' => 'U0E591Z7Z',
+        'ivometz' => 'U05084NHP',
         'jakobbuis' => 'U3AK7MC1K',
+        'julesverdijk' => 'UCF2LQ9EJ',
+        'remcolakens' => 'U7T5ZGDDJ',
+        'remkojanse' => 'U62EV881Z',
+        'sanderwapstra' => 'U1LUAPRJP',
+        'sebastiaanhols' => 'UHEKCMEMN',
+        'stephandebruin' => 'UB0UV282E',
+        'stevenotto' => 'U22RGSV5H',
     ];
 
     public static function map(string $username) : string
