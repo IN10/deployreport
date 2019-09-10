8a991691690b22a2c03127f3c58519ecae7ebc80
Jakob Buis <jakob.buis@in10.nl>
2019-09-10 11:48:05 +0200

    fix naming in slack message

--- resources/views/slack/deployment_message.blade.php
+++ resources/views/slack/deployment_message.blade.php
@@ -1,4 +1,4 @@
-{!! $user !!} just deployed to {{ $stage }}. {{ $messages->count() === 0 ? 'No messages were mentioned in this release.' : 'The following messages were mentioned in this release:' }}
+{!! $user !!} just deployed to {{ $stage }}. {{ $messages->count() === 0 ? 'No tickets were mentioned in this release.' : 'The following tickets were mentioned in this release:' }}
 @if ($messages->count() > 0)
 @foreach ($messages as $message)
 * {{ $message }}
