b88217486458a0a9d982e8cea4ea34779849a6e7
Jakob Buis <jakob@jakobbuis.nl>
2019-09-21 21:52:13 +0200

    WIP of slack callbacks

---
+++ app/Http/Controllers/SlackController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SlackController extends Controller
+{
+    public function options(Request $request) : JsonResponse
+    {
+    }
+
+    public function request(Request $request) : JsonResponse
+    {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::post('/deploys', 'DeploysController@create');
+
+Route::post('slack/options', 'SlackController@options');
+Route::post('slack/request', 'SlackController@request');
